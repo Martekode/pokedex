feb835734ee9358061e9f5767b06f7bcbb6b42ef
Martekode <BrianDesmedt9000@hotmail.com>
2022-05-23 16:27:27 +0200

    bijne gelukt om evolutions correct weer te geven, geeft enkel previous evolution

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const url = 'https://pokeapi.co/api/v2/pokemon/';
+const evoUrl = 'https://pokeapi.co/api/v2/pokemon-species/';
 let input = document.getElementById('inputUser');
 let searchBtn = document.getElementById('btnUser');
 let nameDisplay = document.getElementById('name');
@@ -7,10 +8,12 @@ let IDDisplay = document.getElementById('ID');
 let pokeInfoDispley = document.getElementById('pokeInfo');
 let pokeAbilitiesDisplay = document.getElementById('abilities');
 let evolutionNameDisplay = document.getElementById('evolutionName');
-let evolutionImgDisplay = document.getElementById('evolustionSprite');
+let evolutionImgDisplay = document.getElementById('evolutionSprite');
 
 searchBtn.addEventListener('click',async function(){
     pokeAbilitiesDisplay.innerHTML = "";
+    evolutionImgDisplay.innerHTML ="";
+    evolutionNameDisplay.innerHTML= "";
     const response  = await fetch(url+input.value);
     let data = await response.json();
     console.log(data);
@@ -24,7 +27,33 @@ searchBtn.addEventListener('click',async function(){
         pokeAbilitiesDisplay.innerHTML += `<li>${data.moves[i].move.name}</li>`;
     }
 
+    const nextEvoUrlFetch = await fetch(evoUrl+data.name);
+    let dataNextEvo = await nextEvoUrlFetch.json();
+    console.log(dataNextEvo);
+    const urlChainfetch = dataNextEvo.evolution_chain.url;
+    const nextEvolutionFetch = await fetch(urlChainfetch);
+    let dataNewEvo = await nextEvolutionFetch.json();
+    console.log(dataNewEvo);
+    if (dataNextEvo.eveolves_from_species === null){
+        let evoName = dataNewEvo.chain.evolves_to[0].species.name;
+        evolutionNameDisplay.innerText = `evolves to or from: ${evoName}`;
 
+        const evoFetch = await fetch(url + evoName);
+        let dataEvo = await evoFetch.json();
+        console.log(dataEvo)
+        evolutionImgDisplay.setAttribute('src' , dataEvo.sprites.front_default);
+        
+    }
+    else{
+        let evoName = dataNextEvo.evolves_from_species.name;
+        evolutionNameDisplay.innerText = `evolves to or from: ${evoName}`;
+        const evoFetch = await fetch(url + evoName);
+        let dataEvo = await evoFetch.json();
+        console.log(dataEvo)
+        evolutionImgDisplay.setAttribute('src' , dataEvo.sprites.front_default);
+    }
+   
+      
 })
 
 

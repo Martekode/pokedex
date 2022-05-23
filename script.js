a64c6bfa8fd782951bf6aa193e01cfed79521562
Martekode <BrianDesmedt9000@hotmail.com>
2022-05-23 11:40:26 +0200

    added better url= more pokemons

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const url = 'https://pokeapi.co/api/v2/pokemon/';
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
 let input = document.getElementById('inputUser');
 let searchBtn = document.getElementById('btnUser');
 let nameDisplay = document.getElementById('name');
 let imgDisplay = document.getElementById('sprite');
+
+
+
+
 searchBtn.addEventListener('click',async function(){
-    const response  = await fetch(url+input.value);
+    const response  = await fetch(url);
     let data = await response.json();
     console.log(data);
-    nameDisplay.innerText = data.name;
-    imgDisplay.setAttribute('src' , data.sprites.front_default)
+    console.log(data.results[input.value].url);
+    nameDisplay.innerText = data.results[input.value].name;
+
+    const responseInput = await fetch(data.results[input.value].url);
+    let dataInput = await responseInput.json();
+
+    console.log(dataInput);
+    imgDisplay.setAttribute('src' , dataInput.sprites.front_default)
 })
 
 

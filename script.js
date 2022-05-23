420216cce408086c776b3d9c2dd154c3910bfe92
Martekode <BrianDesmedt9000@hotmail.com>
2022-05-23 15:03:27 +0200

    changed it to search on name, i did it wrong from the start

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
+const url = 'https://pokeapi.co/api/v2/pokemon/';
 let input = document.getElementById('inputUser');
 let searchBtn = document.getElementById('btnUser');
 let nameDisplay = document.getElementById('name');
@@ -6,26 +6,25 @@ let imgDisplay = document.getElementById('sprite');
 let IDDisplay = document.getElementById('ID');
 let pokeInfoDispley = document.getElementById('pokeInfo');
 let pokeAbilitiesDisplay = document.getElementById('abilities');
+let evolutionNameDisplay = document.getElementById('evolutionName');
+let evolutionImgDisplay = document.getElementById('evolustionSprite');
+
 searchBtn.addEventListener('click',async function(){
     pokeAbilitiesDisplay.innerHTML = "";
-    const response  = await fetch(url);
+    const response  = await fetch(url+input.value);
     let data = await response.json();
     console.log(data);
-    console.log(data.results[input.value].url);
-    nameDisplay.innerText = data.results[input.value].name;
+    nameDisplay.innerText = data.name;
     
-
-    const responseInput = await fetch(data.results[input.value].url);
-    let dataInput = await responseInput.json();
-
-    console.log(dataInput);
-    imgDisplay.setAttribute('src' , dataInput.sprites.front_default)
-    IDDisplay.innerText = `PokéID: ${dataInput.id}`;
+    imgDisplay.setAttribute('src' , data.sprites.front_default);
+    IDDisplay.innerText = `PokéID: ${data.id}`;
 
     
     for (let i = 0 ;i<4;i++){
-        pokeAbilitiesDisplay.innerHTML += `<li>${dataInput.moves[i].move.name}</li>`;
+        pokeAbilitiesDisplay.innerHTML += `<li>${data.moves[i].move.name}</li>`;
     }
+
+
 })
 
 
